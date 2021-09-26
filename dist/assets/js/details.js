411da916fe4a94791fbdefe065856c21d8642acf
ndanglan <nbh381891@gmail.com>
2021-09-27 02:35:34 +0700

    update 27-09

--- dist/assets/js/details.js
+++ dist/assets/js/details.js
@@ -10,6 +10,7 @@
         movePerNavItem = navItems[0].clientHeight,
         index1 = 0,
         index2 = 0;
+        console.log(movePerNavItem);
 
     nextBtn.addEventListener('click', ()=>{
         if(index1 >= mainItems.length -1){
@@ -21,7 +22,7 @@
         mainContainer.style.transition = '0.5s';
         
         index2 ++;
-        if(index2 >= 11){
+        if(index2 >= 12){
             return;
         }
         navSlide.style.transform = `translateY(${-index2*movePerNavItem}px)`;
@@ -29,7 +30,7 @@
     });
 
     prevBtn.addEventListener('click', ()=>{
-        if(index1 <= 0){
+        if(index1 < 0){
             return;
         }
         index1 --;
@@ -37,10 +38,25 @@
         mainContainer.style.transform = `translateX(${-index1*movePerMainItem}px)`;
         mainContainer.style.transition = '0.5s';
         index2 --;
-        if(index2 <=0 || index2 >=11){
+        if(index2 <0 || index2 >=12){
             return;
         }
         navSlide.style.transform = `translateY(${-index2*movePerNavItem}px)`;
         navSlide.style.transition = '0.5s';
+    })
+
+    Array.from(navItems).forEach((e)=>{
+        e.addEventListener('click', function(){
+            index1 = this.getAttribute('data-index');
+            index2 = this.getAttribute('data-index');
+            mainContainer.style.transform = `translateX(${-index1*movePerMainItem}px)`;
+            mainContainer.style.transition = '0.5s';
+
+            if(index2 >=12){
+                return;
+            }
+            navSlide.style.transform = `translateY(${-index2*movePerNavItem}px)`;
+            navSlide.style.transition = '0.5s';
+        })
     })
 })();
