48664fb35ea9b40bfe923a732232f2b8683d79cf
ndanglan <nbh381891@gmail.com>
2021-09-27 02:52:27 +0700

    update 09/27

--- dist/assets/js/details.js
+++ dist/assets/js/details.js
@@ -4,29 +4,31 @@
         nextBtn = document.querySelector(".control.right"),
         prevBtn = document.querySelector(".control.left"),
         navSlide = document.querySelector(".product-nav-container"),
-        navItems = document.querySelectorAll(".product-slide-nav .item");
+        navItems = document.querySelectorAll(".product-slide-nav .item"),
+        colorOptions = document.querySelectorAll(".color-option");
 
     let movePerMainItem = mainItems[0].clientWidth,
         movePerNavItem = navItems[0].clientHeight,
         index1 = 0,
         index2 = 0;
-        console.log(movePerNavItem);
+
+    let moveItem = function(container,movePer,index,direction){
+        container.style.transform = `translate${direction}(${-index*movePer}px)`;
+        container.style.transition = '0.5s';
+    }
 
     nextBtn.addEventListener('click', ()=>{
         if(index1 >= mainItems.length -1){
             return;
         }
         index1 ++;
-        
-        mainContainer.style.transform = `translateX(${-index1*movePerMainItem}px)`;
-        mainContainer.style.transition = '0.5s';
-        
+        moveItem(mainContainer,movePerMainItem,index1,"X");
+
         index2 ++;
         if(index2 >= 12){
             return;
         }
-        navSlide.style.transform = `translateY(${-index2*movePerNavItem}px)`;
-        navSlide.style.transition = '0.5s';
+        moveItem(navSlide,movePerNavItem,index2,"Y");
     });
 
     prevBtn.addEventListener('click', ()=>{
@@ -34,29 +36,38 @@
             return;
         }
         index1 --;
-        
-        mainContainer.style.transform = `translateX(${-index1*movePerMainItem}px)`;
-        mainContainer.style.transition = '0.5s';
+        moveItem(mainContainer,movePerMainItem,index1,"X");
+
         index2 --;
         if(index2 <0 || index2 >=12){
             return;
         }
-        navSlide.style.transform = `translateY(${-index2*movePerNavItem}px)`;
-        navSlide.style.transition = '0.5s';
+        moveItem(navSlide,movePerNavItem,index2,"Y");
     })
 
     Array.from(navItems).forEach((e)=>{
         e.addEventListener('click', function(){
             index1 = this.getAttribute('data-index');
             index2 = this.getAttribute('data-index');
-            mainContainer.style.transform = `translateX(${-index1*movePerMainItem}px)`;
-            mainContainer.style.transition = '0.5s';
 
+            moveItem(mainContainer,movePerMainItem,index1,"X");
             if(index2 >=12){
                 return;
             }
-            navSlide.style.transform = `translateY(${-index2*movePerNavItem}px)`;
-            navSlide.style.transition = '0.5s';
+            moveItem(navSlide,movePerNavItem,index2,"Y");
+        })
+    })
+
+    Array.from(colorOptions).forEach((e)=>{
+        e.addEventListener('click', function(){
+            index1 = this.getAttribute('color-index');
+            index2 = this.getAttribute('color-index');
+
+            moveItem(mainContainer,movePerMainItem,index1,"X");
+            if(index2 >=12){
+                index2 = 11; 
+            }
+            moveItem(navSlide,movePerNavItem,index2,"Y");
         })
     })
 })();
